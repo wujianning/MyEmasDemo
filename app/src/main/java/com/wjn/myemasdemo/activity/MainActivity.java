e85d90b692fab56272b4595b5fff9054dbd7e2e6
wujianning <tmac324421@126.com>
2019-01-11 10:06:39 +0800

    Initial commit

---
+++ app/src/main/java/com/wjn/myemasdemo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wjn.myemasdemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wjn.myemasdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
